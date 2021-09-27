3d0ba59c3843977b17ea90fc537120652fe04356
guoqi <guoqi76327@163.com>
2021-09-27 11:28:35 +0800

    1.封装请求

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module fegin-go
 
 go 1.17
+
+require (
+	github.com/go-resty/resty/v2 v2.6.0 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+)
--- tool/request.go
+++ tool/request.go
@@ -1,46 +1,31 @@
 package tool
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"io/ioutil"
+	"github.com/go-resty/resty/v2"
 	"log"
-	"net/http"
-	"time"
 )
 
 // Get request
 func Get(url string) string {
-	client := &http.Client{Timeout: 5 * time.Second}
-	response, err := client.Get(url)
+	var err error
+	client := resty.New()
+	resp, err := client.R().
+		EnableTrace().
+		Get(url)
 	if err != nil {
 		log.Panicf("the request is fail url %s and err message %s", url, err.Error())
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(response.Body)
-	result, _ := ioutil.ReadAll(response.Body)
-	return string(result)
+	return string(resp.Body())
 }
 
 // Post request
 func Post(url string, data interface{}, contentType string) string {
-	client := &http.Client{Timeout: 5 * time.Second}
-	jsonData, _ := json.Marshal(data)
-	resp, err := client.Post(url, contentType, bytes.NewBuffer(jsonData))
+	var err error
+	client := resty.New()
+	//client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.R().EnableTrace().SetBody(data).SetHeader("Content-Type", contentType).Post(url)
 	if err != nil {
 		log.Panicf("the request is fail url %s and err message %s", url, err.Error())
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(resp.Body)
-	result, _ := ioutil.ReadAll(resp.Body)
-	return string(result)
+	return string(resp.Body())
 }
